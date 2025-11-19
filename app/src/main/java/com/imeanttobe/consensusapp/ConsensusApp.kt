7b42a9b181c35a87e4630de93b5544d5bae8bc36
Shawn Kang <csm06063@naver.com>
2025-11-20 00:52:26 +0900

    chore: separate ConsensusApp from MainActivity

---
+++ app/src/main/java/com/imeanttobe/consensusapp/ConsensusApp.kt
@@ -0,0 +1,7 @@
+package com.imeanttobe.consensusapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ConsensusApp : Application()
--- app/src/main/java/com/imeanttobe/consensusapp/MainActivity.kt
+++ app/src/main/java/com/imeanttobe/consensusapp/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.imeanttobe.consensusapp
 
-import android.app.Application
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,14 +10,9 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.imeanttobe.consensusapp.seal.NativeLib
 import com.imeanttobe.consensusapp.ui.theme.ConsensusTheme
 import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class ConsensusApp : Application()
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
