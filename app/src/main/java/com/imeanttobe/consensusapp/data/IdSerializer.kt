1276ad1856c0227a764e275af1b3e6bfaa6adfcd
Shawn Kang <77564014+i-meant-to-be@users.noreply.github.com>
2025-11-22 00:54:59 +0900

    [CHORE] Proto Datastore 도입 (#10)
    * chore: add Protobuf and datastore dependencies
    
    * feat: add proto file and serializer for user id
    
    * feat: add a simple test code for Proto datastore
    
    * chore: misc
    
    * refactor: change the type of serializer exception
    
    * chore: update .gitignore

---
+++ app/src/main/java/com/imeanttobe/consensusapp/data/IdSerializer.kt
@@ -0,0 +1,34 @@
+package com.imeanttobe.consensusapp.data
+
+import android.content.Context
+import androidx.datastore.core.CorruptionException
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.Serializer
+import androidx.datastore.dataStore
+import com.imeanttobe.consensusapp.Id
+import java.io.InputStream
+import java.io.OutputStream
+
+const val FILE_NAME = "id.pb"
+
+object IdSerializer : Serializer<Id> {
+    override val defaultValue: Id
+        get() = Id.getDefaultInstance()
+
+    override suspend fun readFrom(input: InputStream): Id {
+        try {
+            return Id.parseFrom(input)
+        } catch (exception: Exception) {
+            throw CorruptionException("Cannot read proto.", exception)
+        }
+    }
+
+    override suspend fun writeTo(t: Id, output: OutputStream) {
+        t.writeTo(output)
+    }
+}
+
+val Context.idDataStore: DataStore<Id> by dataStore(
+    fileName = FILE_NAME,
+    serializer = IdSerializer
+)
