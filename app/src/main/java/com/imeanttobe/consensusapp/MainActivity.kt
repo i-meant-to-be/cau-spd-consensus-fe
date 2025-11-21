e682ec5bcb531ec96617ec605452c6f48f4787fd
Shawn Kang <csm06063@naver.com>
2025-11-21 22:56:10 +0900

    feat: add a simple test code for Proto datastore

--- app/src/main/java/com/imeanttobe/consensusapp/MainActivity.kt
+++ app/src/main/java/com/imeanttobe/consensusapp/MainActivity.kt
@@ -1,33 +1,37 @@
 package com.imeanttobe.consensusapp
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import com.imeanttobe.consensusapp.data.idDataStore
 import com.imeanttobe.consensusapp.seal.NativeLib
 import com.imeanttobe.consensusapp.ui.theme.ConsensusTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val messageFromCpp = NativeLib.stringFromJNI()
 
         enableEdgeToEdge()
         setContent {
             ConsensusTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = messageFromCpp,
-                        modifier = Modifier.padding(innerPadding),
-                    )
+                    Greeting(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -36,11 +40,34 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(
-    name: String,
     modifier: Modifier = Modifier,
 ) {
-    Text(
-        text = name,
-        modifier = modifier,
-    )
+    val messageFromCpp = NativeLib.stringFromJNI()
+    val context = LocalContext.current
+    val data = context.idDataStore.data.collectAsState(initial = Id.getDefaultInstance())
+    val coroutineScope = rememberCoroutineScope()
+
+    Column {
+        Text(
+            text = messageFromCpp,
+            modifier = modifier,
+        )
+
+        Text(
+            text = data.value.id,
+            modifier = modifier,
+        )
+
+        Button(
+            onClick = {
+                coroutineScope.launch {
+                    context.idDataStore.updateData {
+                        it.toBuilder().setId("Proto Datastore attached!").build()
+                    }
+                }
+            },
+        ) {
+            Text(text = "Update")
+        }
+    }
 }
