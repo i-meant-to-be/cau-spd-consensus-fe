6c3f8b8e4d58a247f44ac5b5122ea9853d3ff637
Shawn Kang <csm06063@naver.com>
2025-11-23 22:31:49 +0900

    feat: implement splash screen

--- app/src/main/java/com/imeanttobe/consensusapp/ui/splash/SplashScreen.kt
+++ app/src/main/java/com/imeanttobe/consensusapp/ui/splash/SplashScreen.kt
@@ -1,9 +1,58 @@
 package com.imeanttobe.consensusapp.ui.splash
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularWavyProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import com.imeanttobe.consensusapp.core.UiState
+import com.imeanttobe.consensusapp.navigation.Route
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
-fun SplashScreen() {
-    Text("Splash Screen")
+fun SplashScreen(
+    navController: NavHostController,
+    viewModel: SplashViewModel = hiltViewModel()
+) {
+    LaunchedEffect(key1 = viewModel.splashState.value) {
+        if (viewModel.splashState.value is UiState.Success) {
+            navController.navigate(Route.DevRoute) {
+                popUpTo(Route.SplashScreen) {
+                    inclusive = true
+                }
+            }
+        }
+    }
+
+    Scaffold { innerPadding ->
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+        ) {
+            // Title
+            Text(
+                text = "Consensus",
+                color = MaterialTheme.colorScheme.primary,
+                style = MaterialTheme.typography.headlineLarge,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+
+            // Loading indicator
+            CircularWavyProgressIndicator()
+        }
+    }
 }
