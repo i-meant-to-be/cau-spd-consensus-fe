e636dbc5f0e047bc4f0606bce1eb2328423e4041
Shawn Kang <csm06063@naver.com>
2025-11-23 16:23:09 +0900

    feat: add main navigation graph into MainActivity

--- app/src/main/java/com/imeanttobe/consensusapp/MainActivity.kt
+++ app/src/main/java/com/imeanttobe/consensusapp/MainActivity.kt
@@ -1,26 +1,12 @@
 package com.imeanttobe.consensusapp
 
-import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import com.imeanttobe.consensusapp.data.idDataStore
-import com.imeanttobe.consensusapp.seal.NativeLib
+import com.imeanttobe.consensusapp.ui.ConsensusNavGraph
 import com.imeanttobe.consensusapp.ui.theme.ConsensusTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -30,44 +16,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ConsensusTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(modifier = Modifier.padding(innerPadding))
-                }
+                ConsensusNavGraph()
             }
-        }
-    }
-}
-
-@Composable
-fun Greeting(
-    modifier: Modifier = Modifier,
-) {
-    val messageFromCpp = NativeLib.stringFromJNI()
-    val context = LocalContext.current
-    val data = context.idDataStore.data.collectAsState(initial = Id.getDefaultInstance())
-    val coroutineScope = rememberCoroutineScope()
-
-    Column {
-        Text(
-            text = messageFromCpp,
-            modifier = modifier,
-        )
-
-        Text(
-            text = data.value.id,
-            modifier = modifier,
-        )
-
-        Button(
-            onClick = {
-                coroutineScope.launch {
-                    context.idDataStore.updateData {
-                        it.toBuilder().setId("Proto Datastore attached!").build()
-                    }
-                }
-            },
-        ) {
-            Text(text = "Update")
         }
     }
 }
