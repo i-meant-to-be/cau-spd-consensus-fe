2fe7a8575ebc7fb52371caf689b4adf38a5aafac
Shawn Kang <csm06063@naver.com>
2025-11-23 22:29:52 +0900

    feat: add ID repository on Hilt module

---
+++ app/src/main/java/com/imeanttobe/consensusapp/core/HiltModule.kt
@@ -0,0 +1,31 @@
+package com.imeanttobe.consensusapp.core
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import com.imeanttobe.consensusapp.Id
+import com.imeanttobe.consensusapp.data.repo.IdRepo
+import com.imeanttobe.consensusapp.data.repo.IdRepoImpl
+import com.imeanttobe.consensusapp.data.serializer.idDataStore
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class HiltModule {
+    companion object {
+        @Provides
+        @Singleton
+        fun provideIdDataStore(@ApplicationContext context: Context): DataStore<Id> {
+            return context.idDataStore
+        }
+    }
+
+    @Binds
+    @Singleton
+    abstract fun bindIdRepo(idRepoImpl: IdRepoImpl): IdRepo
+}
