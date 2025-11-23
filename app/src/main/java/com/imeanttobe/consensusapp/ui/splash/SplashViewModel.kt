c33fa3ae55b843dfab7ecd98a227bbedf449f9d2
Shawn Kang <csm06063@naver.com>
2025-11-23 22:31:39 +0900

    feat: add ID init logics on splash view model

---
+++ app/src/main/java/com/imeanttobe/consensusapp/ui/splash/SplashViewModel.kt
@@ -0,0 +1,42 @@
+package com.imeanttobe.consensusapp.ui.splash
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.imeanttobe.consensusapp.core.UiState
+import com.imeanttobe.consensusapp.data.repo.IdRepo
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import java.util.UUID
+import javax.inject.Inject
+
+@HiltViewModel
+class SplashViewModel @Inject constructor(
+    private val idRepo: IdRepo,
+) : ViewModel() {
+    private val _splashState = mutableStateOf<UiState<Unit>>(UiState.Idle)
+    val splashState: State<UiState<Unit>> = _splashState
+
+    init {
+        checkAndGenerateId()
+    }
+
+    fun checkAndGenerateId() {
+       viewModelScope.launch {
+           _splashState.value = UiState.Loading
+
+           try {
+               if (idRepo.isExist()) {
+                   _splashState.value = UiState.Success(Unit)
+               } else {
+                   val id = UUID.randomUUID().toString()
+                   idRepo.setId(id)
+                   _splashState.value = UiState.Success(Unit)
+               }
+           } catch (e: Exception) {
+               _splashState.value = UiState.Failure(e.message ?: "Unknown error")
+           }
+       }
+    }
+}
