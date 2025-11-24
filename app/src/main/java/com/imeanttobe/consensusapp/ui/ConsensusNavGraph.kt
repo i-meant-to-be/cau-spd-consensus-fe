3c6020984955c63419660bae9df80d7451ba0f3b
Shawn Kang <77564014+i-meant-to-be@users.noreply.github.com>
2025-11-24 13:02:49 +0900

    [FEAT] 기본 UI 구조 준비 (#16)
    * feat: add a file for planned screens
    
    * feat: register new pages on nav graph

--- app/src/main/java/com/imeanttobe/consensusapp/ui/ConsensusNavGraph.kt
+++ app/src/main/java/com/imeanttobe/consensusapp/ui/ConsensusNavGraph.kt
@@ -8,10 +8,14 @@ import androidx.navigation.navDeepLink
 import androidx.navigation.toRoute
 import com.imeanttobe.consensusapp.BuildConfig
 import com.imeanttobe.consensusapp.navigation.Route
+import com.imeanttobe.consensusapp.ui.create_poll.CreatePollScreen
 import com.imeanttobe.consensusapp.ui.dev.DevScreen
 import com.imeanttobe.consensusapp.ui.home.HomeScreen
+import com.imeanttobe.consensusapp.ui.host_dashboard.HostDashboardScreen
+import com.imeanttobe.consensusapp.ui.result.ResultScreen
 import com.imeanttobe.consensusapp.ui.splash.SplashScreen
 import com.imeanttobe.consensusapp.ui.vote.VoteScreen
+import com.imeanttobe.consensusapp.ui.waiting.WaitingScreen
 
 @Composable
 fun ConsensusNavGraph() {
@@ -19,8 +23,14 @@ fun ConsensusNavGraph() {
 
     NavHost(
         navController = navController,
-        startDestination = Route.SplashScreen
+        startDestination = Route.SplashScreen,
     ) {
+        if (BuildConfig.IS_DEV_MODE_ENABLED) {
+            composable<Route.DevRoute> {
+                DevScreen(navController = navController)
+            }
+        }
+
         composable<Route.SplashScreen> {
             SplashScreen(navController = navController)
         }
@@ -29,23 +39,35 @@ fun ConsensusNavGraph() {
             HomeScreen()
         }
 
-        if (BuildConfig.IS_DEV_MODE_ENABLED) {
-            composable<Route.DevRoute> {
-                DevScreen()
-            }
+        composable<Route.CreatePollScreen> {
+            CreatePollScreen()
         }
 
         composable<Route.VoteScreen>(
-            deepLinks = listOf(
-                navDeepLink {
-                    uriPattern = "consensus://poll/{pollId}"
-                }
-            )
+            deepLinks =
+                listOf(
+                    navDeepLink {
+                        uriPattern = "consensus://poll/{pollId}"
+                    },
+                ),
         ) { backStackEntry ->
             val route: Route.VoteScreen = backStackEntry.toRoute()
-
             VoteScreen(pollId = route.pollId)
         }
 
+        composable<Route.ResultScreen> { backStackEntry ->
+            val route: Route.ResultScreen = backStackEntry.toRoute()
+            ResultScreen(pollId = route.pollId)
+        }
+
+        composable<Route.HostDashboardScreen> { backStackEntry ->
+            val route: Route.HostDashboardScreen = backStackEntry.toRoute()
+            HostDashboardScreen(pollId = route.pollId)
+        }
+
+        composable<Route.WaitingScreen> { backStackEntry ->
+            val route: Route.WaitingScreen = backStackEntry.toRoute()
+            WaitingScreen(pollId = route.pollId)
+        }
     }
 }
--- app/src/main/java/com/imeanttobe/consensusapp/ui/dev/DevScreen.kt
+++ app/src/main/java/com/imeanttobe/consensusapp/ui/dev/DevScreen.kt
@@ -10,25 +10,55 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import com.imeanttobe.consensusapp.navigation.Route
 import java.util.UUID
 
 @Composable
-fun DevScreen(viewModel: DevViewModel = hiltViewModel()) {
+fun DevScreen(
+    navController: NavHostController,
+    viewModel: DevViewModel = hiltViewModel(),
+) {
     Scaffold { innerPadding ->
         Column(
-            modifier = Modifier
-                .padding(innerPadding)
-                .fillMaxSize()
-                .padding(16.dp),
+            modifier =
+                Modifier
+                    .padding(innerPadding)
+                    .fillMaxSize()
+                    .padding(16.dp),
         ) {
             Text(text = viewModel.userId.value)
 
             Button(
                 onClick = {
                     viewModel.setId(UUID.randomUUID().toString())
-                }
+                },
             ) {
                 Text(text = "Set ID")
+            }
+
+            Button(onClick = { navController.navigate(Route.CreatePollScreen) }) {
+                Text(text = "Create Poll")
+            }
+
+            Button(onClick = { navController.navigate(Route.HomeRoute) }) {
+                Text(text = "Home")
+            }
+
+            Button(onClick = { navController.navigate(Route.VoteScreen(pollId = "ID")) }) {
+                Text(text = "Vote")
+            }
+
+            Button(onClick = { navController.navigate(Route.ResultScreen(pollId = "ID")) }) {
+                Text(text = "Result")
+            }
+
+            Button(onClick = { navController.navigate(Route.WaitingScreen(pollId = "ID")) }) {
+                Text(text = "Waiting")
+            }
+
+            Button(onClick = { navController.navigate(Route.HostDashboardScreen(pollId = "ID")) }) {
+                Text(text = "Host Dashboard")
             }
         }
     }
