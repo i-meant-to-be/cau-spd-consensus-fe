e070a22f4561fd3288dd5bfb899eadea8d186887
Shawn Kang <shineksu813@outlook.com>
2025-11-27 14:32:55 +0900

    feat: implement sub Composables

---
+++ app/src/main/java/com/imeanttobe/consensusapp/ui/create_poll/components/AddOptionDialog.kt
@@ -0,0 +1,54 @@
+package com.imeanttobe.consensusapp.ui.create_poll.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AddOptionDialog(
+    newOption: String,
+    onNewOptionChange: (String) -> Unit,
+    onConfirm: () -> Unit,
+    onDismiss: () -> Unit
+) {
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        title = { Text(text = "새 후보 추가") },
+        text = {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = "새로 추가할 후보를 입력해주세요."
+                )
+                OutlinedTextField(
+                    value = newOption,
+                    onValueChange = onNewOptionChange,
+                    label = { Text(text = "후보") },
+                    singleLine = true,
+                    maxLines = 1,
+                    placeholder = { Text(text = "홍길동") },
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onConfirm) {
+                Text(text = "추가")
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = onConfirm) {
+                Text(text = "취소")
+            }
+        }
+    )
+}
---
+++ app/src/main/java/com/imeanttobe/consensusapp/ui/create_poll/components/InputItemDescription.kt
@@ -0,0 +1,62 @@
+package com.imeanttobe.consensusapp.ui.create_poll.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun InputItemDescription(
+    title: String,
+    icon: ImageVector,
+    description: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier
+    ) {
+        // Title
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                imageVector = icon,
+                tint = MaterialTheme.colorScheme.primary,
+                contentDescription = null
+            )
+            Text(
+                text = title,
+                color = MaterialTheme.colorScheme.primary,
+                style = MaterialTheme.typography.titleMedium,
+            )
+        }
+
+        // Description
+        Text(
+            text = description,
+            style = MaterialTheme.typography.bodyMedium,
+        )
+    }
+}
+
+@Preview
+@Composable
+fun InputItemDescriptionPreview() {
+    InputItemDescription(
+        title = "투표 제목",
+        icon = Icons.Outlined.Add,
+        description = "투표의 제목을 입력해주세요."
+    )
+}
