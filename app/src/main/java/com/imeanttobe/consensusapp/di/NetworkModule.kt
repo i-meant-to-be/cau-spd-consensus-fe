4c9f47c75e31ee6518febe3e62e9e631850f3e79
Shawn Kang <shineksu813@outlook.com>
2025-12-04 15:28:18 +0900

    fix: add @Singleton annotation on OkHttp client

--- app/src/main/java/com/imeanttobe/consensusapp/di/NetworkModule.kt
+++ app/src/main/java/com/imeanttobe/consensusapp/di/NetworkModule.kt
@@ -16,7 +16,7 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
     @Provides
-//    @Singleton
+    @Singleton
     fun provideOkHttpClient(
         authInterceptor: AuthInterceptor
     ): OkHttpClient {
