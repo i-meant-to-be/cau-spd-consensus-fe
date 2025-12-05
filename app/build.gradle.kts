dd8ab0c4dd168391525ab0726b24d07df3c5f265
Shawn Kang <77564014+i-meant-to-be@users.noreply.github.com>
2025-12-06 01:33:21 +0900

    [CHORE] API 모방 환경 변수 추가 (#33)
    chore: add local property `IS_MOCK_ENABLED`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,12 +57,14 @@ android {
         // Set the values from local.properties
         val isDevModeEnabled = parseBoolean(localProperties.getProperty("config.isDevModeEnabled"), false)
         val isHttpLoggingEnabled = parseBoolean(localProperties.getProperty("config.isHttpLoggingEnabled"), false)
+        val isMockEnabled = parseBoolean(localProperties.getProperty("config.isMockEnabled"), false)
         val apiBaseUrl = localProperties.getProperty("api.baseUrl")
             ?: throw GradleException("API_BASE_URL is not set in local.properties")
 
         // Set the buildConfigField
         buildConfigField("Boolean", "IS_DEV_MODE_ENABLED", isDevModeEnabled.toString())
         buildConfigField("Boolean", "IS_HTTP_LOGGING_ENABLED", isHttpLoggingEnabled.toString())
+        buildConfigField("Boolean", "IS_MOCK_ENABLED", isMockEnabled.toString())
         buildConfigField("String", "API_BASE_URL", "\"$apiBaseUrl\"")
     }
 
