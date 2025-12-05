ad53cbb85a6b4b15f2e563e238157c66ee3d9a99
Shawn Kang <shineksu813@outlook.com>
2025-12-05 14:25:31 +0900

    refactor: add getter for cached value on interface

--- app/src/main/java/com/imeanttobe/consensusapp/data/repo/IdRepo.kt
+++ app/src/main/java/com/imeanttobe/consensusapp/data/repo/IdRepo.kt
@@ -4,4 +4,6 @@ interface IdRepo {
     suspend fun getId(): Result<String>
     suspend fun setId(id: String): Result<Boolean>
     suspend fun isExist(): Result<Boolean>
+
+    fun getCachedIdOrEmpty(): String
 }
