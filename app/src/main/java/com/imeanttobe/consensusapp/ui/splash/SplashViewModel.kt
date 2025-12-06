d6c91d4c3e118f28f3f29ba7a0b7738f621090ee
Shawn Kang <csm06063@naver.com>
2025-12-06 23:03:22 +0900

    refactor: replace mutableStateOf with MutableStateFlow on view model

--- app/src/main/java/com/imeanttobe/consensusapp/ui/splash/SplashViewModel.kt
+++ app/src/main/java/com/imeanttobe/consensusapp/ui/splash/SplashViewModel.kt
@@ -1,12 +1,13 @@
 package com.imeanttobe.consensusapp.ui.splash
 
-import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.imeanttobe.consensusapp.core.UiState
 import com.imeanttobe.consensusapp.domain.repo.IdRepo
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import java.util.UUID
 import javax.inject.Inject
@@ -15,11 +16,11 @@ import javax.inject.Inject
 class SplashViewModel @Inject constructor(
     private val idRepo: IdRepo,
 ) : ViewModel() {
-    private val _splashState = mutableStateOf<UiState<Unit>>(UiState.Idle)
-    val splashState: State<UiState<Unit>> = _splashState
+    private val _splashState = MutableStateFlow<UiState<Unit>>(UiState.Idle)
+    val splashState: StateFlow<UiState<Unit>> = _splashState
 
-    private val _dialogState = mutableStateOf<Boolean>(false)
-    val dialogState: State<Boolean> = _dialogState
+    private val _dialogState = MutableStateFlow<Boolean>(false)
+    val dialogState: StateFlow<Boolean> = _dialogState
 
     init {
         checkAndGenerateId()
@@ -32,27 +33,34 @@ class SplashViewModel @Inject constructor(
     fun checkAndGenerateId() {
        viewModelScope.launch {
            _splashState.value = UiState.Loading
+           Log.d("Consensus", "# checkAndGenerateId called.")
 
            try {
                val isExist = idRepo.isExist()
 
                if (isExist.isSuccess) {
                    if (isExist.getOrDefault(false)) {
+                       Log.d("Consensus", "# ID exists.")
                        _splashState.value = UiState.Success(Unit)
                    } else {
+                       Log.d("Consensus", "# ID does not exists.")
                        val id = UUID.randomUUID().toString()
                        val result = idRepo.setId(id)
 
                        if (result.isSuccess) {
+                           Log.d("Consensus", "# Successfully created new id.")
                            _splashState.value = UiState.Success(Unit)
                        } else {
+                           Log.e("Consensus", "# Failed to create new id.")
                            _splashState.value = UiState.Failure(result.exceptionOrNull()?.message ?: "새로 생성된 ID를 쓰던 중 오류 발생")
                        }
                    }
                } else {
+                   Log.e("Consensus", "# Failed to check whether the id is exists.")
                    _splashState.value = UiState.Failure(isExist.exceptionOrNull()?.message ?: "ID를 확인하는 중 오류 발생")
                }
            } catch (e: Exception) {
+               Log.e("Consensus", "# Failed to check while generating id.")
                _splashState.value = UiState.Failure(e.message ?: "ID를 설정하는 중 오류 발생")
            }
        }
