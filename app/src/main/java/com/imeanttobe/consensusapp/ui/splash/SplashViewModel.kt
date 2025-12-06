4005dbece077fcd48bee8b4a1334f49d4dc3b61e
Shawn Kang <csm06063@naver.com>
2025-12-06 23:12:47 +0900

    chore: remove log function calls

--- app/src/main/java/com/imeanttobe/consensusapp/ui/splash/SplashViewModel.kt
+++ app/src/main/java/com/imeanttobe/consensusapp/ui/splash/SplashViewModel.kt
@@ -40,27 +40,21 @@ class SplashViewModel @Inject constructor(
 
                if (isExist.isSuccess) {
                    if (isExist.getOrDefault(false)) {
-                       Log.d("Consensus", "# ID exists.")
                        _splashState.value = UiState.Success(Unit)
                    } else {
-                       Log.d("Consensus", "# ID does not exists.")
                        val id = UUID.randomUUID().toString()
                        val result = idRepo.setId(id)
 
                        if (result.isSuccess) {
-                           Log.d("Consensus", "# Successfully created new id.")
                            _splashState.value = UiState.Success(Unit)
                        } else {
-                           Log.e("Consensus", "# Failed to create new id.")
                            _splashState.value = UiState.Failure(result.exceptionOrNull()?.message ?: "새로 생성된 ID를 쓰던 중 오류 발생")
                        }
                    }
                } else {
-                   Log.e("Consensus", "# Failed to check whether the id is exists.")
                    _splashState.value = UiState.Failure(isExist.exceptionOrNull()?.message ?: "ID를 확인하는 중 오류 발생")
                }
            } catch (e: Exception) {
-               Log.e("Consensus", "# Failed to check while generating id.")
                _splashState.value = UiState.Failure(e.message ?: "ID를 설정하는 중 오류 발생")
            }
        }
