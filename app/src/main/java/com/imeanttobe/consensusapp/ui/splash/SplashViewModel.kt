be304e0fe3a85519065455a2603112afb3a8f785
Shawn Kang <csm06063@naver.com>
2025-12-06 23:35:14 +0900

    feat: add security params setting step on SplashScreen

--- app/src/main/java/com/imeanttobe/consensusapp/ui/splash/SplashViewModel.kt
+++ app/src/main/java/com/imeanttobe/consensusapp/ui/splash/SplashViewModel.kt
@@ -1,14 +1,19 @@
 package com.imeanttobe.consensusapp.ui.splash
 
-import android.util.Log
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.imeanttobe.consensusapp.core.UiState
 import com.imeanttobe.consensusapp.domain.repo.IdRepo
+import com.imeanttobe.consensusapp.seal.NativeLib
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.util.UUID
 import javax.inject.Inject
 
@@ -22,47 +27,81 @@ class SplashViewModel @Inject constructor(
     private val _dialogState = MutableStateFlow<Boolean>(false)
     val dialogState: StateFlow<Boolean> = _dialogState
 
+    private val _loadingMessage = MutableStateFlow<String>("대기")
+    val loadingMessage: StateFlow<String> = _loadingMessage
+
     init {
-        checkAndGenerateId()
+        initApp()
     }
 
     fun setDialogState(state: Boolean) {
         _dialogState.value = state
     }
 
-    fun checkAndGenerateId() {
-       viewModelScope.launch {
-           _splashState.value = UiState.Loading
-           Log.d("Consensus", "# checkAndGenerateId called.")
+    fun initApp() {
+        viewModelScope.launch {
+            _splashState.value = UiState.Loading
 
-           try {
-               val isExist = idRepo.isExist()
+            try {
+                // 1. Prepare SEAL
+                _loadingMessage.value = "보안 환경을 준비하는 중..."
+                val isSealReady = withContext(Dispatchers.IO) {
+                    NativeLib.initContext()
+                }
+                if (!isSealReady) {
+                    _splashState.value = UiState.Failure("보안 환경 구성에 실패했습니다.")
+                    _loadingMessage.value = "오류 발생"
+                    return@launch
+                }
 
-               if (isExist.isSuccess) {
-                   if (isExist.getOrDefault(false)) {
-                       Log.d("Consensus", "# ID exists.")
-                       _splashState.value = UiState.Success(Unit)
-                   } else {
-                       Log.d("Consensus", "# ID does not exists.")
-                       val id = UUID.randomUUID().toString()
-                       val result = idRepo.setId(id)
+                delay(1000)
+                
+                // 2. Prepare id
+                _loadingMessage.value = "기기 정보를 확인하는 중..."
+                val idResult = withContext(Dispatchers.IO) {
+                    processIdCheck()
+                }
 
-                       if (result.isSuccess) {
-                           Log.d("Consensus", "# Successfully created new id.")
-                           _splashState.value = UiState.Success(Unit)
-                       } else {
-                           Log.e("Consensus", "# Failed to create new id.")
-                           _splashState.value = UiState.Failure(result.exceptionOrNull()?.message ?: "새로 생성된 ID를 쓰던 중 오류 발생")
-                       }
-                   }
-               } else {
-                   Log.e("Consensus", "# Failed to check whether the id is exists.")
-                   _splashState.value = UiState.Failure(isExist.exceptionOrNull()?.message ?: "ID를 확인하는 중 오류 발생")
-               }
-           } catch (e: Exception) {
-               Log.e("Consensus", "# Failed to check while generating id.")
-               _splashState.value = UiState.Failure(e.message ?: "ID를 설정하는 중 오류 발생")
-           }
-       }
+                delay(1000)
+
+                if (idResult.isSuccess) {
+                    _splashState.value = UiState.Success(Unit)
+                    _loadingMessage.value = "준비 완료!"
+                } else {
+                    _loadingMessage.value = "오류 발생"
+                    _splashState.value = UiState.Failure(idResult.exceptionOrNull()?.message ?: "알 수 없는 오류")
+                }
+            } catch (e: Exception) {
+                _loadingMessage.value = "오류 발생"
+                _splashState.value = UiState.Failure(e.message ?: "알 수 없는 오류")
+            }
+        }
+    }
+
+    private suspend fun processIdCheck(): Result<Unit> {
+        return try {
+            val isExistResult = idRepo.isExist()
+
+            if (isExistResult.isSuccess) {
+                val isExist = isExistResult.getOrDefault(false)
+
+                if (isExist) {
+                    Result.success(Unit)
+                } else {
+                    val newId = UUID.randomUUID().toString()
+                    val createResult = idRepo.setId(newId)
+
+                    if (createResult.isSuccess) {
+                        Result.success(Unit)
+                    } else {
+                        Result.failure(Exception(createResult.exceptionOrNull()?.message))
+                    }
+                }
+            } else {
+                return Result.failure(Exception(isExistResult.exceptionOrNull()?.message))
+            }
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
     }
 }
