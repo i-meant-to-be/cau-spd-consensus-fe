05e0824b5f204634ab221c11486514920922923e
Shawn Kang <csm06063@naver.com>
2025-12-07 16:49:07 +0900

    fix: change return type of encryption/decryption function to nullable

--- app/src/main/java/com/imeanttobe/consensusapp/seal/NativeLib.kt
+++ app/src/main/java/com/imeanttobe/consensusapp/seal/NativeLib.kt
@@ -8,6 +8,6 @@ object NativeLib {
     external fun stringFromJNI(): String
     external fun initContext(): Boolean
     external fun generateKeys(): SealKeys?
-    external fun encrypt(inputVector: LongArray): Boolean
-    external fun decrypt(cipherBytes: ByteArray): Boolean
+    external fun encrypt(inputVector: LongArray): ByteArray?
+    external fun decrypt(cipherBytes: ByteArray): LongArray?
 }
